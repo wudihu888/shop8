f362a6953e7f25a988f20146b695ccc665f5d4d1
tiger <1816601660@qq.com>
2018-03-12 15:17:14 +0800

    添加SET

--- application/back/controller/Brand.php
+++ application/back/controller/Brand.php
@@ -9,37 +9,76 @@ use think\Session;
 use think\Paginator;
 class Brand extends Controller
 {
-    public function add(){
 
-        //判断是什么请求 get 展示数据  POST 添加数据
+    //合并添加和更新操作
+    // public function set($id=null){
+    //     $request = Request::instance();
+    //     //判断是否有ID  有ID就是更新  没有就是添加
+    //     if(is_null($id)){
+    //         $model = new BrandModel();
+
+    //     }else{
+    //         $model = BrandModel::get($id);
+
+    //     }
+    //     if($request->isGet()){
+    //         $data = Session::has('data') ? Session::get('data') :$model->getData();
+            
+    //         return $this->fetch('set',['message'=>Session::get('message'),'data'=>$data]);
+    //     }elseif($request->isPost()){
+            
+    //         $data = $request->post();
+            
+    //         $validate = Loader::validate('BrandAddValidate');
+    //         $result = $validate->batch(true)->check($data);
+    //         if(!$result){
+    //             return $this->redirect('set',[],302,['message'=>$validate->getError(),'data'=>$data]);
+    //         }
+    //         $model->data($data);
+    //         $model->save();
+    //         return $this->redirect('index');
+            
+    //     }
+
+    // }
+
+    /*
+    *   合并添加更新数据
+    */
+    public function set($id=null){
         $request = Request::instance();
+        if(is_null($id)){
+            //ID 为空 表示添加数据
+            $model = new BrandModel();
+        }else{
+            $model = BrandModel::get($id);
+        }
+        
         if($request->isGet()){
-            return $this->fetch('add',['message'=>Session::get('message')]);
-        }elseif($request->isPost()){
-            $data = $request->post();
 
-            //验证
+            $data = Session::has('data') ? Session::get('data') : $model->getData();
+              return  $this->fetch('set',['message'=>Session::get('message'),'data'=>$data]);
+        }
+        elseif($request->isPost()){
+            $data = $request->post();
             $validate = Loader::validate('BrandAddValidate');
             $result = $validate->batch(true)->check($data);
-           
             if(!$result){
-              $this->redirect('add',[],'302',['message'=>$validate->getError()]);
+                return $this->redirect('set',[],302,['message'=>$validate->getError(),'data'=>$data]);
             }
-
-            $model= new BrandModel();
-            $model->data($data);//data 方法添加数据
+            $model->data($data);
             $model->save();
-            
-            $this->redirect('index');
-
+            return $this->redirect('index');
         }
-
-        
     }
+
+
+
+   
     // 删除操作
     public function delete(){
 
-        $a = input('selected/a',[]);
+      
      
 
         BrandModel::destroy(input('selected/a',[]));
@@ -48,9 +87,6 @@ class Brand extends Controller
         $this->redirect('index');
     }
 
-    public function set(){
-        
-    }
 
 
 
